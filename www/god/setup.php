b5bc06e20c7b4833054bdcaa5c85b40836294037
Blaine Cook <romeda@gmail.com>
2011-12-01 02:30:28 +0000

    a very simplistic first-pass at web-based database setup.

---
+++ www/god/setup.php
@@ -0,0 +1,37 @@
+<?php
+
+    include("include/init.php");
+
+    $tables = db_fetch("SHOW TABLES");
+
+    // There aren't any tables, so this is initial setup. Create the database, and away we go.
+    if ( $tables['ok'] && count($tables['rows']) == 0 ) {
+
+        $schema = file_get_contents(FLAMEWORK_WWW_DIR . "/../schema/db_main.schema");
+        $schema_queries = preg_split('/;/', $schema);
+
+        $GLOBALS['smarty']->assign('error', false);
+
+        foreach ($schema_queries as $query) {
+
+            $result = _db_query($query, 'main');
+
+            if (!$result['ok']) {
+              $GLOBALS['smarty']->assign('error', $result);
+              break;
+            }
+        }
+
+        $GLOBALS['smarty']->display("page_god_setup.txt");
+
+    } else {
+
+        // There are tables in the database, so don't let anyone touch anything.
+        error_404();
+
+    }
+
+
+    exit();
+
+?>
